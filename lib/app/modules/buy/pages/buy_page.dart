7fe6df803f524c1606893f1b4832a38cff6b98b6
mr-santos <mr-santos@debian-BULLSEYE-live-builder-AMD64>
2023-07-16 16:25:50 -0300

    perfumando

--- lib/app/modules/buy/pages/buy_page.dart
+++ lib/app/modules/buy/pages/buy_page.dart
@@ -8,7 +8,6 @@ import 'package:rei_da_bola/design_system/widgets/widget_loading.dart';
 import 'package:rei_da_bola/shared/api/state_response.dart';
 
 import '../../../../design_system/images/images_app.dart';
-import '../models/buy_model.dart';
 import 'components/card_buy_players.dart';
 
 class BuyPage extends StatefulWidget {
@@ -32,8 +31,6 @@ class _BuyPageState extends State<BuyPage> {
   Widget build(BuildContext context) {
     
     final colors = ColorsAppDefault();
-    //List<BuyModel> list = controllerBuy.groupItemsByAbb(controllerBuy.listBuy);
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: colors.green,
@@ -54,66 +51,36 @@ class _BuyPageState extends State<BuyPage> {
         ),
       ),
       body: 
-          Observer(
-            builder: (_) => /*FutureBuilder(
-              future: controllerBuy.initBuy(), // Coloque a chamada do método initBuy() dentro do FutureBuilder
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  // Exibir o indicador circular enquanto estiver carregando
-                  return Center(
-                    child: WidgetLoading(
-                      color: colors.green,
-                      width: 6,
-                      thickness: 1,
-                    ),
-                  );
-                } 
-                else {
-                  return ListView.builder(
-                    itemCount: list.length,
-                    itemBuilder: (context, index){
-                      /*return ListTile(
-                        title: Text(list[index].player!.firstname!),
-                        subtitle: Text(list[index].player!.position!.name!),
-                      );*/
-                      int numImage = controllerBuy.generateNumber();
-                      return CardBuyPlayers(
-                        color: colors,
-                        image: controllerBuy.image[numImage],
-                        namePlayer: list[index].player!.firstname!,
-                        nameTeam: list[index].teamEdition!.team!.name!,
-                        positionAbb: list[index].player!.position!.abb!,
-                      );
-                    }
-                  );
-                }
-              }
-            ),*/
-            controllerBuy.stateController != StateResponse.sucess ? Center(
-                    child: WidgetLoading(
-                      color: colors.green,
-                      width: 6,
-                      thickness: 1,
-                    ),
-                  ) 
-                  : ListView.builder(
-                    itemCount: controllerBuy.listBuy.length,
-                    itemBuilder: (context, index){
-                      int numImage = controllerBuy.generateNumber();
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 8.0, right: 8.0, left: 8.0),
-                        child: CardBuyPlayers(
-                          color: colors,
-                          numb: index,
-                          image: controllerBuy.image[numImage],
-                          namePlayer: '${controllerBuy.listBuy[index].player!.firstname!} ${controllerBuy.listBuy[index].player!.lastname!}',
-                          nameTeam: controllerBuy.listBuy[index].teamEdition!.team!.name!,
-                          positionAbb: controllerBuy.listBuy[index].player!.position!.abb!,
-                        ),
-                      );
-                    }
+        Observer(
+          builder: (_) => 
+          controllerBuy.stateController != StateResponse.sucess 
+            ? 
+            Center(
+              child: WidgetLoading(
+                color: colors.green,
+                width: 6,
+                thickness: 1,
+              ),
+            ) 
+            : 
+            ListView.builder(
+              itemCount: controllerBuy.listBuy.length,
+              itemBuilder: (context, index){
+                int numImage = controllerBuy.generateNumber();
+                return Padding(
+                  padding: const EdgeInsets.only(top: 8.0, right: 8.0, left: 8.0),
+                  child: CardBuyPlayers(
+                    color: colors,
+                    numb: index,
+                    image: controllerBuy.image[numImage],
+                    namePlayer: '${controllerBuy.listBuy[index].player!.firstname!} ${controllerBuy.listBuy[index].player!.lastname!}',
+                    nameTeam: controllerBuy.listBuy[index].teamEdition!.team!.name!,
+                    positionAbb: controllerBuy.listBuy[index].player!.position!.abb!,
                   ),
-          ),
+                );
+              }
+            ),
+        ),
     );
   }
 }
